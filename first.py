4f18cb429f6822a81ae4fea9695484888aed13f2
online-poaty <refoh.divad@gmail.com>
2020-05-05 15:25:04 +0200

    commit02

--- first.py
+++ first.py
@@ -1,5 +1,20 @@
 import tweepy
 import time
+
+
+def getLastSeenId(fileName):
+    f_r = open(fileName, 'r')
+    lsID = int(f_r.read().strip())
+    f_r.close()
+    return lsID
+
+def setLastSeenId(id, fileName):
+    f_w = open(fileName, 'w')
+    f_w.write(str(id))
+    f_w.close()
+    return
+
+
 CONSUMER_KEY = 'UqfYkk074wKd9DSEFDA0JeBwZ'
 CONSUMER_SECRET = 'WfMtfFD2VXaI8XDaRgSNYHUDh4V4MJ0KGTMb1TM7b6RpYXrTFm'
 ACCESS_KEY = '1256980054141284356-KOHIO95m9HSszgum6GsOWlYpaxRdO3'
@@ -9,12 +24,14 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-lastSeenId = 1257300822095990786
+#lastSeenId = 1257300822095990786
+
+FILENAME_lsID = 'last_seen_id.txt'
+SCREEN_NAME = '@CaroKann20'
 
 
+run = True
 
-run  = True
-f = open('controller.txt', 'r')
 
 #home = api.home_timeline()
 
@@ -33,17 +50,39 @@ for m in home:
     print(str(m.id) + ': ' + m.text)"""
 i = 0
 while run:
+    lastSeenId = getLastSeenId(FILENAME_lsID)
     mentions = api.mentions_timeline(lastSeenId)
     if len(mentions) > 0:
-        lastSeenId = mentions[0].id
-    for m in mentions:
-        print('iteration ' + str(i) + ' - ' + str(m.id) + ': ' + m.text)
-        api.update_status("wdym " + "%s  @%s" % (m.text, m.author.screen_name), m.id)
-    if f.mode == 'r':
-        content = f.read()
-        if not content.__contains__('running'):
-            run = False
-    i += 1
+        setLastSeenId(mentions[0].id,FILENAME_lsID)
+        for m in mentions:
+            print('iteration ' + str(i) + ' - ' + str(m.id) + ': ' + m.text)
+            api.update_status("wdym " + "%s @%s" % (m.text.replace(SCREEN_NAME, ''), m.author.screen_name), m.id)
+    f_cont = open('controller.txt', 'r')
+    content = f_cont.read()
+    if 'running' not in content:
+        run = False
+    f_cont.close()
     time.sleep(5)
+    i += 1
 
-print("program finished, last seen: " + str(lastSeenId))
+print("program finished, last seen: " + str(getLastSeenId(FILENAME_lsID)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
